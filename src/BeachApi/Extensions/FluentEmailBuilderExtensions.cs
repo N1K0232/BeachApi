b3639c5ba4b2dc1bd55db288b26aba213826afd7
N1K0232 <ns.nicolasilvestri@libero.it>
2024-02-29 19:47:21 +0100

    Add fluent email

---
+++ src/BeachApi/Extensions/FluentEmailBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using BeachApi.BusinessLayer.RemoteServices;
+using FluentEmail.Core.Interfaces;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace BeachApi.Extensions;
+
+public static class FluentEmailBuilderExtensions
+{
+    public static FluentEmailServicesBuilder AddSendinblueSender(this FluentEmailServicesBuilder builder)
+    {
+        builder.Services.TryAddSingleton<ISender, SendinblueSender>();
+        return builder;
+    }
+}
