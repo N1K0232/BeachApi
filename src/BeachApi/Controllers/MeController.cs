faa6373c7022b65cac1e231621605c2b2f6c0aee
N1K0232 <ns.nicolasilvestri@libero.it>
2024-03-01 18:10:37 +0100

    Add method to get the authenticated user

---
+++ src/BeachApi/Controllers/MeController.cs
@@ -0,0 +1,26 @@
+﻿using BeachApi.BusinessLayer.Services.Interfaces;
+using BeachApi.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using OperationResults.AspNetCore;
+
+namespace BeachApi.Controllers;
+
+public class MeController : ControllerBase
+{
+    private readonly IAuthenticatedService authenticatedService;
+
+    public MeController(IAuthenticatedService authenticatedService)
+    {
+        this.authenticatedService = authenticatedService;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> Get()
+    {
+        var result = await authenticatedService.GetAsync();
+        return HttpContext.CreateResponse(result);
+    }
+}
