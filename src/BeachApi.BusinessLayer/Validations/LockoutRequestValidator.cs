235c8609f7c33677d71fe952f7127caef8af7781
N1K0232 <ns.nicolasilvestri@libero.it>
2024-03-02 21:51:33 +0100

    Add validator for requesting a lockout

---
+++ src/BeachApi.BusinessLayer/Validations/LockoutRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using BeachApi.Shared.Models.Requests;
+using FluentValidation;
+
+namespace BeachApi.BusinessLayer.Validations;
+
+public class LockoutRequestValidator : AbstractValidator<LockoutRequest>
+{
+    public LockoutRequestValidator()
+    {
+        RuleFor(l => l.UserId)
+            .NotEmpty()
+            .WithMessage("the user is required");
+
+        RuleFor(l => l.LockoutEnd)
+            .NotEmpty()
+            .WithMessage("the lockout end is required");
+    }
+}
